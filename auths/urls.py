ca9cc548b2550b8788159876d2865b629cf75c7a
Elvis Mugisha <mugishaelvis104@gmail.com>
2025-12-10 11:02:01 +0200

    feat: Initialize Django project with comprehensive settings, authentication app, and Docker configuration.

--- auths/urls.py
+++ auths/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path("register/", views.UserRegistrationView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
+    path(
+        "token/refresh/", views.CustomTokenRefreshView.as_view(), name="token-refresh"
+    ),
     path("verify-email/", views.EmailVerificationView.as_view(), name="verify-email"),
     path("resend-otp/", views.ResendOTPView.as_view(), name="resend-otp"),
     path("users/", views.UserListView.as_view(), name="user-list"),
