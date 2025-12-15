4451127e94e28d848cd3ae86589ff7c659672c8c
Elvis Mugisha <mugishaelvis104@gmail.com>
2025-12-15 17:29:54 +0200

    feat: Implement currency management API with CRUD operations, bulk creation, permissions, and pagination.

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from django.db import transaction as db_transaction
+from django.db import models, transaction as db_transaction
 from django.utils import timezone
 
 from utils import loggings
